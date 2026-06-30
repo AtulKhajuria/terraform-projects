430f4bc23e2d547b05b365ab16c834b5e14722c8
AtulKhajuria <atulkhajuria234@gmail.com>
2026-06-30 15:28:50 +0530

    Modified main.tf file - added resources

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -1,3 +1,20 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "terraform-igw"
+  }
+}
+resource "aws_subnet" "public_subnet_1" {
+    vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_1_cidr
+}
+resource "aws_subnet" "public_subnet_2" {
+    vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_2_cidr
 }
