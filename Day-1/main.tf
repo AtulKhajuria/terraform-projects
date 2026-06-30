ff989bdc5cb528c6fb97e5ce1d0d957b45eb6467
AtulKhajuria <atulkhajuria234@gmail.com>
2026-06-30 16:01:39 +0530

    Modified main.tf - Added route_table and it's associations

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -18,3 +18,22 @@ resource "aws_subnet" "public_subnet_2" {
     vpc_id = aws_vpc.main.id
   cidr_block = var.public_subnet_2_cidr
 }
+resource "aws_route_table" "public_rt" {
+    vpc_id = aws_vpc.main.id
+    tags = {
+      Name = "public-route-table"
+    }
+}
+resource "aws_route" "internet_access" {
+  route_table_id = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+resource "aws_route_table_association" "public_subnet_1" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id = aws_subnet.public_subnet_1.id
+}
+resource "aws_route_table_association" "public_subnet_2" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id = aws_subnet.public_subnet_2.id
+}
