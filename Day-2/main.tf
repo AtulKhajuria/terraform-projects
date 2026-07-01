3ee33109c7f0d4a661f0e8281573e17d8b8ddd63
AtulKhajuria <atulkhajuria234@gmail.com>
2026-07-02 02:39:05 +0530

    Fixed issue in modules file - Modules in Terraform

--- Day-2/main.tf
+++ Day-2/main.tf
@@ -2,5 +2,5 @@ module "network" {
   source = "./modules/network"
   vpc_cidr = var.vpc_cidr
   public_subnet_1_cidr = var.public_subnet_1_cidr
-  public_subnet_2_cidr = var.public_subnet_1_cidr
+  public_subnet_2_cidr = var.public_subnet_2_cidr
 }
