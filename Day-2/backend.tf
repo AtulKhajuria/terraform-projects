31390fa6666ba2ad3e9df0e68fe13b30d1d88288
AtulKhajuria <atulkhajuria234@gmail.com>
2026-07-04 02:06:01 +0530

    Added S3 backend and DynamoDB lock on state

---
+++ Day-2/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "atul-terraform-state-2026"
+    key            = "terraform-projects/Day-2/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
