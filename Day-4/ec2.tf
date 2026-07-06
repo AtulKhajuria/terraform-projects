65697ccec77b3f42f9965e17bd948bfb4f78598b
AtulKhajuria <atulkhajuria234@gmail.com>
2026-07-07 02:33:57 +0530

    Fixed issue in ami_name

--- Day-4/ec2.tf
+++ Day-4/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web_server" {
-  ami = data.aws_ami.ubuntu
+  ami = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   subnet_id = module.network.public_subnet_1_cidr
   vpc_security_group_ids = [ aws_security_group.allow_traffic.id ]
