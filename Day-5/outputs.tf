0f2a42e8c551aca846980bcfc2afe53377afcd55
AtulKhajuria <atulkhajuria234@gmail.com>
2026-07-09 02:11:06 +0530

    Added dns output in outputs.tf file

--- Day-5/outputs.tf
+++ Day-5/outputs.tf
@@ -9,3 +9,7 @@ output "private_ip" {
 output "public_dns" {
   value = aws_instance.web_server.public_dns
 }
+output "alb_dns_name" {
+  description = "Application Load Balancer DNS Name"
+  value       = aws_lb.web_alb.dns_name
+}
