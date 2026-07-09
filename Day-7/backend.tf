fddb556f4db9a2fb46695310095b294de3e288d4
AtulKhajuria <atulkhajuria234@gmail.com>
2026-07-10 02:35:46 +0530

    Updated backend.tf

--- Day-7/backend.tf
+++ Day-7/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket         = "atul-terraform-state-2026"
-    key            = "terraform-projects/Day-6/terraform.tfstate"
+    key            = "terraform-projects/Day-7/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
