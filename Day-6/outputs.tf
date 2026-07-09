63dea9a0dd0c045905db15c59bde8d1e013b8ea2
AtulKhajuria <atulkhajuria234@gmail.com>
2026-07-09 18:33:45 +0530

    Updated Outputs.tf

--- Day-6/outputs.tf
+++ Day-6/outputs.tf
@@ -1,14 +1,14 @@
-output "public_ip" {
-  value = aws_instance.web_server.public_ip
-}
+# output "public_ip" {
+#   value = aws_instance.web_server.public_ip
+# }
 
-output "private_ip" {
-  value = aws_instance.web_server.private_ip
-}
+# output "private_ip" {
+#   value = aws_instance.web_server.private_ip
+# }
 
-output "public_dns" {
-  value = aws_instance.web_server.public_dns
-}
+# output "public_dns" {
+#   value = aws_instance.web_server.public_dns
+# }
 output "alb_dns_name" {
   description = "Application Load Balancer DNS Name"
   value       = aws_lb.web_alb.dns_name
