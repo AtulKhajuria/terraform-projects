84260c99e5bb0e7d7074abc62e35c0d88c681a32
ATUL KHAJURIA <84517651+AtulKhajuria@users.noreply.github.com>
2026-07-13 16:34:30 +0530

    Update user_data.sh

--- Day-7/user_data.sh
+++ Day-7/user_data.sh
@@ -3,4 +3,4 @@ apt update -y
 apt install nginx -y
 systemctl enable nginx
 systemctl start nginx
-echo "<h1>Hello from Terraform</h1>" > /var/www/html/index.html
+echo "<h1>Hello from Terraform ${hostname}</h1>" > /var/www/html/index.html
